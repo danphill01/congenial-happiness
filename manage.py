435dd70cefacac50241bd1f9716cfcab592792f1
Daniel Phillips <daniel.j.phillips@gmail.com>
2018-03-06 11:01:51 -0800

    remove bucketitem import

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from app import app, db, models
-from app.models import User, Bucket, BucketItem
+from app.models import User, Bucket
 import unittest
 import coverage
 import os
